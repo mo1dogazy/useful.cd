4931a86d853dac801114c5413dcdb61d040e74d9
MOLDOGAZY KABYLBEKOV <moldogazy.kabylbekov.kk@gmail.com>
2023-02-13 19:55:29 +0600

    Added UseState.jsx file

---
+++ ReactJS/UseState.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function UseStateNice() {
+  const [greeting, setGreeting] = useState({ greet: "Nice Hellow World!" });
+  console.log(greeting, setGreeting);
+
+  function updateGreeting() {
+    const newGreeting = { ...greeting };
+    newGreeting.greet = "Nice Oh sorry, Hi from MOLDOGAZY aka!";
+    setGreeting(newGreeting);
+  }
+  return (
+    <div className="all">
+      <h1>{greeting.greet}</h1>
+      <button onClick={updateGreeting}>Update greeting</button>
+    </div>
+  );
+}
+
+export default UseStateNice;
